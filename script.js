105501089fea664180cbe3bfd6f11b25cc5d197f
jaypeagi <git@average-joe.info>
2014-02-05 12:24:41 +0000

    First
    from Codepen.io

---
+++ script.js
@@ -0,0 +1,15 @@
+function setup() {
+  var h = decodeURIComponent(window.location.hash.replace("#", ''));
+  var n = $("#name").text();
+  if(h && h != n) {
+    $("#name").text(h);
+  }
+  $("#url").text(window.location);
+  document.title = n + " is Sinking!";
+}
+
+$("#name").on("input",function() {
+  window.location.hash = encodeURIComponent($("#name").text());
+});
+$(window).on("hashchange", setup);
+setup();
